a95eec2b89ae2ec395aecdc2ca815713c7529bbb
jimirwinjr <snymax@gmail.com>
2022-11-12 18:47:32 -0500

    For some reason I am still getting an internal protection level error so I need to figure out how I can handle these better

--- Sources/SwiftSVG/User Interface/SXSVGView.swift
+++ Sources/SwiftSVG/User Interface/SXSVGView.swift
@@ -8,7 +8,10 @@
 import SwiftUI
 
 public struct SXSVGView : View {
-    public init() {}
+    public var value: Int
+    
+    //This is just a test
+    public init(_ value: Int) {}
     public var body: some View {
         ZStack{
             Text("SVG VIEW")
